9a3674b1cc1365d5860a064a6df8663e6accad19
Antipov Alexander <antipovaleksandr95@gmail.com>
2019-10-09 13:45:31 +0300

    fixed code

--- app/src/main/java/com/antipov/fastlane/App.kt
+++ app/src/main/java/com/antipov/fastlane/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
